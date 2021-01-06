9173bc8afe06b0f78563bec8ef22db3f97f74fa5
Shivam <mshivam76@gmail.com>
2021-01-06 15:29:20 +0530

    CORS fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   apiHost: 'http://udagram-mishra-dev-dev2.ap-south-1.elasticbeanstalk.com/api/v0'
+  // apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
